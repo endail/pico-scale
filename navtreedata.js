1293c6d56a69f39396cee167698833238816281e
endail <52652357+endail@users.noreply.github.com>
2022-08-06 14:06:48 +0000

    Deploying to gh-pages from  @ 122a323636a6519e0efbf02477566c6e3e90cd6c 🚀

--- navtreedata.js
+++ navtreedata.js
@@ -23,9 +23,13 @@ for the JavaScript code in this file
 var NAVTREE =
 [
   [ "pico-scale", "index.html", [
-    [ "Clone Repository", "index.html#autotoc_md1", null ],
-    [ "How to Use", "index.html#autotoc_md2", null ],
-    [ "How to Calibrate", "index.html#autotoc_md3", null ],
+    [ "Clone", "index.html#autotoc_md1", [
+      [ "Method 1", "index.html#autotoc_md2", null ],
+      [ "Method 2", "index.html#autotoc_md3", null ]
+    ] ],
+    [ "Initialise the HX711", "index.html#autotoc_md4", null ],
+    [ "How to Use the HX711 as a Scale", "index.html#autotoc_md5", null ],
+    [ "How to Calibrate", "index.html#autotoc_md6", null ],
     [ "Data Structures", "annotated.html", [
       [ "Data Structures", "annotated.html", "annotated_dup" ],
       [ "Data Structure Index", "classes.html", null ],
